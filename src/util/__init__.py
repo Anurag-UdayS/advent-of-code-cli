54fed50d5252a995a71ba008e11c28ab4bd97a22
Anurag Tewary <anuragtewary@outlook.com>
2022-12-22 18:33:29 +0530

    Started with DirManager

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,6 +1,10 @@
-# __init__.py
-# Anurag Tewary
-# 12 December, 2022
+""" 
+package util;
 
-# This is the __init__.py file.
-# This is used to render the directory as a package.
+-> __init__.py
+-> Anurag Tewary
+-> 12 December, 2022
+
+	* This is the __init__.py file.
+	* This is used to render the directory as a package.
+"""
--- src/util/directory.py
+++ src/util/directory.py
@@ -1,4 +1,4 @@
-""" 
+"""
 package util;
 
 -> directory.py
@@ -9,25 +9,64 @@ package util;
 	* Creation of Year (Event) / Date (Day) directories.
 """
 
-#DEFINE Path str
-Path = str
+import os
+from pathlib import Path
 
-class DirManager():
-	"""DirManager will be a parent utility, and will help in managing all Directory movement."""
-	def __init__(self: DirManager, path: Path):
+from unittest import TestCase
+import unittest as StartUnitTests
+
+
+class DirManager(object):
+	"""
+	DirManager will be a parent utility, and will
+	help in managing all Directory movement.
+	"""
+
+	instances: dict[Path, object] = {}
+
+	def __new__(cls, year: int, path: Path = Path('.')):
+		path = cls.getYearDirectoryPath(Path, year)
+
+		if path in cls.instances:
+			return cls.instances[path] 
+		else:
+			return super().__new__(cls)
+
+	def __init__(self, path: Path('.')):
 		self.path = path
+		DirManager.instances[path] = self
 
 	@staticmethod
-	def createYearDirectory(year: int, parent: Path = '.') -> DirManager:
+	def getYearDirectoryPath(self, path: Path | str, year: int):
 		pass
 
-	def createDayDirectory(self: DirManager, parent: Path = self.path, ):
+		# If the last part is an int, and it is equal to year,
+		# treat it as the directory, Otherwise append.
+		
+	def createDayDirectory(self, parent: Path = None):
 		pass
 
 
-#################################################
-#                    TESTING                    #
-#################################################
+#################################################################################
+#                                    TESTING                                    #
+#################################################################################
+
+class DirManagerTesting(TestCase):
+
+	# Test 1: assert that directories created with the same path are equal.
+	def testEquality(self):
+		inst1: DirManager = DirManager('./abc/2023')
+		inst2: DirManager = DirManager('./abc/2023')
+		self.assertEqual(inst1, inst2)
+
+	# Test 2: assert that directories created with different paths are inequal.
+	def testInEquality(self):
+		inst1: DirManager = DirManager('./abc/2023')
+		inst2: DirManager = DirManager('./abc/2021')
+		self.assertNotEqual(inst1, inst2)
+
 
 if __name__ == '__main__':
-	pass
+	# StartUnitTests.main()
+	print(Path('abc').is_dir())
+	
