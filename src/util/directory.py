907d4d5dc8dd6e110dac670ae900eaf61a49fb85
Anurag Tewary <anuragtewary@outlook.com>
2022-12-23 21:11:31 +0530

    Added the feature to create concrete paths.

--- src/util/directory.py
+++ src/util/directory.py
@@ -12,6 +12,8 @@ package util;
 import os
 from pathlib import Path
 
+from pathlib import *
+
 from unittest import TestCase
 import unittest as StartUnitTests
 
@@ -24,25 +26,48 @@ class DirManager(object):
 
 	instances: dict[Path, object] = {}
 
-	def __new__(cls, year: int, path: Path = Path('.')):
-		path = cls.getYearDirectoryPath(Path, year)
+	def __new__(cls, year: int, path: Path = Path.cwd()):
+		path = cls.getYearDirectoryPath(path, year)
 
 		if path in cls.instances:
-			return cls.instances[path] 
+			return cls.instances[path]
 		else:
 			return super().__new__(cls)
 
-	def __init__(self, path: Path('.')):
+	def __init__(self, year: int, path: Path.cwd()):
+		path = DirManager.getYearDirectoryPath(path, year)
 		self.path = path
+		self.year = year
 		DirManager.instances[path] = self
 
 	@staticmethod
-	def getYearDirectoryPath(self, path: Path | str, year: int):
-		pass
+	def getYearDirectoryPath(path: Path | str, year: int) -> Path:
+		if (type(path) is str):
+			path = Path(path)
+
+		# Raise an error if a file is passed.
+		if (path.is_dir()):
+			# error
+			return None
+
+		directory: str = path.parts[-1]
+		root: str = path.parts[0]
+
+		# If path has no prefix, or is relative to cwd,
+		# Replace Path(.) with Path(CWD)
+		# Test that 1st char is not /
+		if (path.parts[0] == '.'):
+			path = Path.cwd() / path.relative_to('.')
+		elif (root[0] != '/'):
+			path = Path.cwd() / path
 
 		# If the last part is an int, and it is equal to year,
 		# treat it as the directory, Otherwise append.
-		
+		if (not directory.isdigit() or int(directory) != year):
+			path = path.joinpath(str(year))
+
+		return path
+
 	def createDayDirectory(self, parent: Path = None):
 		pass
 
@@ -55,18 +80,18 @@ class DirManagerTesting(TestCase):
 
 	# Test 1: assert that directories created with the same path are equal.
 	def testEquality(self):
-		inst1: DirManager = DirManager('./abc/2023')
-		inst2: DirManager = DirManager('./abc/2023')
+		inst1: DirManager = DirManager(2023, './abc/2023')
+		inst2: DirManager = DirManager(2023, './abc/2023')
 		self.assertEqual(inst1, inst2)
 
 	# Test 2: assert that directories created with different paths are inequal.
 	def testInEquality(self):
-		inst1: DirManager = DirManager('./abc/2023')
-		inst2: DirManager = DirManager('./abc/2021')
+		inst1: DirManager = DirManager(2023, './abc/2023')
+		inst2: DirManager = DirManager(2021, './abc/2021')
 		self.assertNotEqual(inst1, inst2)
 
 
 if __name__ == '__main__':
-	# StartUnitTests.main()
-	print(Path('abc').is_dir())
-	
+
+	StartUnitTests.main()
+	# print(Path('./abc/2023'))
